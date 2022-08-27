94559e76c0861c8a5413d466a232c762f26b5006
X-N-E-O-N-C-O-D-E <harishashmi1233@gmail.com>
2022-08-27 11:31:34 +0300

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,20 +1,20 @@
 #variable = a container for a value. Behaves as the value that it contains
 
 #string = a series of characters
-first_name = "Bro"
+first_name = "Xneon"
 last_name = "Code"
 full_name = first_name +" "+ last_name
 print("Hello "+full_name)
 # print(type(first_name))
 
 #int = a whole integer
-age = 21
+age = 14
 age += 1
 print("Your age is: "+str(age))
 # print(type(age))
 
 #float = a decimal number
-height = 250.5
+height = 160.6
 print("Your height is: "+str(height)+"cm")
 # print(type(height))
 
